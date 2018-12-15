4b1aae9c16e1e64ab4c0c1136b5760579582e9f4
muhamad rianto <muhtoevill>
2018-12-16 02:15:03 +0700

    Bot Auto Claim sseth v 0.2

--- sseth.php
+++ sseth.php
@@ -5,14 +5,14 @@ function getStr($string,$start,$end){
 	$str = explode($end,($str[1]));
 	return $str[0];
 }
-function claim(){
+function claim($email,$devid){
 	$arr = array("\r","	");
 	$url = "http://sscoinmedia.tech/EthereumWebService/ethereumBalanceUpdate1.php";
 	$h = explode("\n",str_replace($arr,"","Content-Type: application/x-www-form-urlencoded; charset=UTF-8
 	User-Agent: Dalvik/2.1.0 (Linux; U; Android 6.0.1; vivo 1606 Build/MMB29M)
 	Host: sscoinmedia.tech
 	Connection: Keep-Alive"));
-	$body = "email=muhtoevill@gmail.com&devid=865588032922559&claimok=ok&";
+	$body = "email=$email&devid=$devid&claimok=ok&";
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, $url);
 	curl_setopt($ch, CURLOPT_HTTPHEADER, $h);
@@ -23,23 +23,26 @@ function claim(){
 	curl_close($ch);
 	return json_decode($x,true);
 }
-echo "#################\n#  @muhtoevill  #\n#   SGB-Team    #\n#################\n";
+echo "#################\n#  @muhtoevill  #\n#   SGB-Team    #\n#  Binary-Team  #\n#################\n";
+echo "email	:";
+$email = trim(fgets(STDIN));
+echo "devid	:";
+$devid = trim(fgets(STDIN));
 while(TRUE){
-	$submit = claim();
+	$submit = claim($email,$devid);
 	$output = json_encode($submit);
 	$balance = getStr($output,'"message":',',');
 	$balance = getStr($output,'"ubal":',',');
 	$claim = getStr($output,'"claimamt":',',');
 	if(strpos($output,"devid")==true){
-                $text = "Berhasil Balance: $balance Claim : $claim Credit By:Muhtoevill";
+                $text = " Berhasil Balance: $balance Claim : $claim Credit By:Muhtoevill Delay 5 menit";
                 $text1 = "\033[32m".$text."\033[0m";
             }else{
-                $text ="GAGAL";
+                $text =" GAGAL";
                 $text1 = "\033[31m".$text."\033[0m";
-				$a++;
-                
         }
-	echo date('d-m-Y H:i:s') .$text1."\n" ;
+	echo date('d-m-Y H:i:s');
+	echo $text1."\n";
 	sleep(300);
 	
 }
